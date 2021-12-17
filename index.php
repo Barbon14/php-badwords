86e71bab25ae1b62392cbefb0e37aa09aeb84f97
BarbonFisso <barbieri.mat@hotmail.it>
2021-12-17 15:08:06 +0100

    Add: text in body

--- index.php
+++ index.php
@@ -1,18 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hello World</title>
 </head>
+
 <body>
     <?php
 
-        $name= 'Mattia';
+    $name = 'Mattia';
+    $text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores eum nemo corporis odio nulla quisquam provident ex. Nobis beatae aperiam saepe dolorum, laborum non ea labore rem sunt eaque repellendus.';
     ?>
     <h1>
         Hello <?php echo $name ?>
     </h1>
+    <p>
+        <?php echo $text ?>
+    </p>
 </body>
+
 </html>
